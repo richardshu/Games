5ee8f591dafb4ff06d834a7d610a3e2cd5bfa46d
Richard Shu <richardjshu@gmail.com>
2019-03-04 22:21:42 -0500

    Initial commit: render white triangle

---
+++ NYUCodebase/hw1/NYUCodebase/main.cpp
@@ -0,0 +1,69 @@
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+
+#include "ShaderProgram.h"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#ifdef _WINDOWS
+#define RESOURCE_FOLDER ""
+#else
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+
+SDL_Window* displayWindow;
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+	glViewport(0, 0, 640, 360);
+
+	ShaderProgram program;
+	program.Load("vertex.glsl", "fragment.glsl");
+
+	glm::mat4 projectionMatrix = glm::mat4(1.0f);
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+	glm::mat4 viewMatrix = glm::mat4(1.0f);
+	projectionMatrix = glm::ortho(-1.777f, 1.777f, -1.0f, 1.0f, -1.0f, 1.0f);
+	glUseProgram(program.programID);
+
+    SDL_Event event;
+    bool done = false;
+    while (!done) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+                done = true;
+            }
+        }
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		program.SetModelMatrix(modelMatrix);
+		program.SetProjectionMatrix(projectionMatrix);
+		program.SetViewMatrix(viewMatrix);
+		float vertices[] = { 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glEnableVertexAttribArray(program.positionAttribute);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(program.positionAttribute);
+		SDL_GL_SwapWindow(displayWindow);
+
+        //glClear(GL_COLOR_BUFFER_BIT);
+        //SDL_GL_SwapWindow(displayWindow);
+    }
+    
+    SDL_Quit();
+    return 0;
+}
